28df2e1c047e424c938e60d9ddfc3df8e77ca0c5
Delano <122936117+SarmentoDelano@users.noreply.github.com>
2024-10-09 17:57:07 -0300

    Update views.py

--- core/views.py
+++ core/views.py
@@ -14,27 +14,17 @@ class LivroDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Livro.objects.all() 
     serializer_class = LivroSerializer 
     name = "livro-detail"
-    search_fields = ("^name",)
-    ordering_fields = ['name']
-
+    
 class AutorList(generics.ListCreateAPIView): 
     queryset = Autor.objects.all() 
     serializer_class = AutorSerializer 
     name = "autor-list" 
-    
-class AutorDetail(generics.RetrieveUpdateDestroyAPIView): 
-    queryset = Autor.objects.all() 
-    serializer_class = AutorSerializer 
-    name = "autor-detail"
+    search_fields = ("^name",)
+    ordering_fields = ['nome']
 
 class CategoriaList(generics.ListCreateAPIView): 
     queryset = Categoria.objects.all() 
     serializer_class = CategoriaSerializer 
     name = "categoria-list"
     search_fields = ("^name",)
-    ordering_fields = ['name']
-    
-class CategoriaDetail(generics.RetrieveUpdateDestroyAPIView): 
-    queryset = Categoria.objects.all() 
-    serializer_class = CategoriaSerializer 
-    name = "categoria-detail"
+    ordering_fields = ['nome']
