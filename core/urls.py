8cc5d76f2daf57739ea99dcbfcbf0332341dc578
Delano <122936117+SarmentoDelano@users.noreply.github.com>
2024-10-09 17:57:42 -0300

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -5,7 +5,5 @@ urlpatterns = [
     path('livros/', views.LivroList.as_view(), name='livros-list'), 
     path('livros/<int:pk>/', views.LivroDetail.as_view(), name='livro-detail'), 
     path('autor/', views.AutorList.as_view(), name='autor-list'), 
-    path('autor/<int:pk>/', views.AutorDetail.as_view(), name='autor-detail'),
     path('categoria/', views.CategoriaList.as_view(), name='categoria-list'), 
-    path('categoria/<int:pk>/', views.CategoriaDetail.as_view(), name='categoria-detail'),
 ] 
